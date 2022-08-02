f341b0816899c98fdf05d75d9cc5b73316b6cefd
augustinecyr <augustinecyr@gmail.com>
2022-08-02 10:57:12 +0800

    first commit

---
+++ src/test/java/com/sg/day16weather/Day16WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.day16weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day16WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
