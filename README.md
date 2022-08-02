a3fbde60cfcf2d02047dee35e878e7961d1ddade
augustinecyr <augustinecyr@gmail.com>
2022-08-02 12:48:30 +0800

    lunch

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 ## Introduction to JSON
 
+## Reminders
+1. add jedis and json-p dependencies from mvnrepository
 ### Weather Application Exercise
-1. 
+1. Create controller WeatherController
+2. RequestMapping & GetMapping
+3. Exporting API_KEY
+4. WeatherService 
+5. Using POSTMAN / ARC to determine the keys & values for the string query.
+6. Conversion of payload to JSON
+7. Reading the json Object
 
-add jedis and json-p dependencies
--- src/main/java/com/sg/day16weather/controllers/WeatherController.java
+++ src/main/java/com/sg/day16weather/controllers/WeatherController.java
@@ -1,5 +1,7 @@
 package com.sg.day16weather.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.sg.day16weather.models.Weather;
 import com.sg.day16weather.services.WeatherService;
 
 @Controller
@@ -21,8 +24,11 @@ public class WeatherController {
     @GetMapping
     public String getWeather(Model model, @RequestParam String city) {
         weatherSvc.getWeather(city);
+        List<Weather> weather = weatherSvc.getWeather(city);
         model.addAttribute("city", city);
+        model.addAttribute("weather", weather);
         return "weather";
 
     }
+
 }
